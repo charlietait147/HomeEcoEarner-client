a80f7416229984cb20b391dfe520f5a89b64f057
Charlie Tait <charlietait147@gmail.com>
2023-08-26 12:22:48 +0100

    add header template html and import logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,18 @@
+import "./Header.scss";
+import logo from "../../assets/images/HomeEcoEarner-logo.png";
 function Header() {
     return ( 
-        <div> This is the header</div>
+        <header className="header">
+            <div className="header__wrapper">
+                <img src={logo} alt="Home Eco Earner logo" className="header__logo" />
+                <nav className="navigation">
+                <ul className="navigation__list">
+                    <li className="navigation__link">News</li>
+                    <li className="navigation__link">Contact Us</li>
+                </ul>
+                </nav>
+            </div>
+        </header>
      );
 }
 
