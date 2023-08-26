fb208966e86afd6c39c9129e0ca81eb8b552b075
Charlie Tait <charlietait147@gmail.com>
2023-08-26 10:51:32 +0100

    fix element page names in app

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
-    <Route path = "/" element = {<Landingpage />}/>
-    <Route path = "qualify" element = {<Qualifyformpage />}/>
-    <Route path = "admin/login" element = {<Adminloginpage />}/>
-    <Route path = "admin/database" element = {<Admindatabasepage />}/>
-    <Route path = "contact-us" element = {<Contactpage />}/>
-    <Route path = "news" element = {<Newspage />} />
-    <Route path = "*" element = {<PageNotFound />} /> 
+    <Route path = "/" element = {<LandingPage />}/>
+    <Route path = "qualify" element = {<QualifyFormPage />}/>
+    <Route path = "admin/login" element = {<AdminLoginPage />}/>
+    <Route path = "admin/database" element = {<AdminDatabase />}/>
+    <Route path = "contact-us" element = {<ContactPage />}/>
+    <Route path = "news" element = {<NewsPage />} />
+    <Route path = "*" element = {<NotFound />} /> 
     </Routes>
     </BrowserRouter>
     </>
