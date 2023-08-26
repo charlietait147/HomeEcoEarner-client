60b607b29be2d33d39403b1d3aecb22b15c41566
Charlie Tait <charlietait147@gmail.com>
2023-08-26 16:00:36 +0100

    create main body content component and import into landing page

---
+++ src/components/MainBodyContent/MainBodyContent.js
@@ -0,0 +1,7 @@
+function MainBodyContent() {
+    return ( 
+        <div>This is the Main Body Content</div>
+     );
+}
+
+export default MainBodyContent;
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,8 +1,12 @@
 import Header from "../../components/Header/Header";
+import MainBodyContent from "../../components/MainBodyContent/MainBodyContent";
 import "./LandingPage.scss";
 function Landingpage() {
     return ( 
+        <>
         <Header />
+        <MainBodyContent />
+        </>
      );
 }
 
