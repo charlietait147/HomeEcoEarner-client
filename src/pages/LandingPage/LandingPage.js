86512ee31cd93f7ab04d0dd89417e7fb608f7cdc
Charlie Tait <charlietait147@gmail.com>
2023-08-26 12:02:10 +0100

    create header component, add to landing page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,7 +1,11 @@
+import Header from "../../components/Header/Header";
 import "./LandingPage.scss";
 function Landingpage() {
     return ( 
+        <>
+        <Header />
         <div>This is the Landing Page</div>
+        </>
      );
 }
 
