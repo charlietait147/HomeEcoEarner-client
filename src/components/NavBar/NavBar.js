948b53ff9a759ba6ae522b885b4bd26114538f56
Charlie Tait <charlietait147@gmail.com>
2023-08-26 20:36:28 +0100

    change classname format in navbar file and update

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,17 +2,15 @@ import "./NavBar.scss";
 import logo from "../../assets/images/homeecoearner-logo.png";
 function Header() {
     return ( 
-        <header className="header">
-            <div className="header__wrapper">
-                <img src={logo} alt="Home Eco Earner logo" className="header__logo" />
-                <nav className="navigation">
-                <ul className="navigation__list">
-                    <li className="navigation__link">NEWS</li>
-                    <li className="navigation__link">CONTACT US</li>
+        <nav className="nav">
+            <div className="nav__wrapper">
+                <img src={logo} alt="Home Eco Earner logo" className="nav__logo" />
+                <ul className="nav__list">
+                    <li className="nav__link">NEWS</li>
+                    <li className="nav__link">CONTACT US</li>
                 </ul>
-                </nav>
             </div>
-        </header>
+        </nav>
      );
 }
 
