3ce650e30ef0787d1baf601b72449effba605c55
Charlie Tait <charlietait147@gmail.com>
2023-08-27 14:09:38 +0100

    tweak modal html and update styling

--- src/components/WaitingListForm/WaitingListForm.js
+++ src/components/WaitingListForm/WaitingListForm.js
@@ -1,26 +1,29 @@
 import { CProgress, CProgressBar, CForm, CFormInput } from '@coreui/react/';
 import peopleTick from "../../assets/images/people-tick.png";
-
+import "./WaitingListForm.scss";
 
 
 function WaitingListForm() {
     return (
         <aside className="waiting-list">
             <div className="waiting-list__wrapper">
-                <h3 className="waiting-list__title">Let's get you on the Waiting List Today</h3>
-                <CProgress color="info" variant="striped" value={50} height={15}>
-                    <CProgressBar>50%</CProgressBar>
-                </CProgress>
+                <div className="waiting-list__banner">
+                    <h3 className="waiting-list__title">Let's get you on the Waiting List Today</h3>
+                    <CProgress className="waiting-list__progress-bar" color="info" variant="striped" value={50} height={25}>
+                        <CProgressBar className="waiting-list__progress-bar-value">50%</CProgressBar>
+                    </CProgress>
+                </div>
                 <div className="waiting-list__image-container">
                     <img src={peopleTick} alt="3 people with tick sign" className="waiting-list__image" />
                 </div>
                 <div className="waiting-list__form-container">
-                    <CForm>
+                    <CForm className="waiting-list__form">
                         <CFormInput
                             type="text"
                             size="lg"
                             className="waiting-list__form-name-input"
                             placeholder='First name'
+                            label="Your first name"
 
                         />
                         <CFormInput
@@ -28,6 +31,15 @@ function WaitingListForm() {
                             size="lg"
                             className="waiting-list__form-email-input"
                             placeholder='Email'
+                            label="Your email"
+
+                        />
+                        <CFormInput
+                            type="text"
+                            size="lg"
+                            className="waiting-list__form-postcode-input"
+                            placeholder='Postcode'
+                            label="Your postcode"
 
                         />
                         <button type="submit" className="waiting-list__join">JOIN WAITING LIST</button>
