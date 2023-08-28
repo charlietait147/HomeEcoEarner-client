7d7cde9264fcf72f6e7520af3723de580077c361
Charlie Tait <charlietait147@gmail.com>
2023-08-28 15:29:30 +0100

    add admin login form html and import scss file

--- src/components/AdminLoginForm/AdminLoginForm.js
+++ src/components/AdminLoginForm/AdminLoginForm.js
@@ -1,6 +1,15 @@
+import "./AdminLoginForm.scss";
 function AdminLoginForm() {
     return ( 
-        <div>This is the Admin Login Form</div>
+        <form className="admin-login">
+            <div className="admin-login__wrapper">
+                <img src="" alt="" className="admin-login__logo" />
+                <h3 className="admin-login__title">Log In</h3>
+                <input type="text" className="admin-login__input" placeholder = "Username" />
+                <input type="text" className="admin-login__input" placeholder= "Password" />
+                <button className="admin-login__log-in">Log In</button>
+            </div>
+        </form>
      );
 }
 
