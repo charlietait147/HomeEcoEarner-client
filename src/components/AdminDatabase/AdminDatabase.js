a6722fd0c6df6e0781827ee730de90081de6472b
Charlie Tait <charlietait147@gmail.com>
2023-08-30 11:52:46 +0100

    get request complete and render mapping of user data on page

--- src/components/AdminDatabase/AdminDatabase.js
+++ src/components/AdminDatabase/AdminDatabase.js
@@ -1,9 +1,30 @@
 import logo from "../../assets/images/homeecoearner-logo.png";
 import searchIcon from "../../assets/icons/search-24px.svg";
 import "./AdminDatabase.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function AdminDatabase() {
+    const [userList, setUserList] = useState([]);
+
+    const getUsersAll = async () => {
+        try {
+            const response = await axios(`${process.env.REACT_APP_API_URL}/users`);
+            const fetchUsers = response.data
+            console.log(fetchUsers);
+            setUserList(fetchUsers);
+        } catch (error) {
+            console.log("Unable to retrieve users" + error);
+        }
+    }
+
+    useEffect(() => {
+        getUsersAll();
+    }, [])
+
+
     return (
+
         <section className="admin-database">
             <div className="admin-database__wrapper">
                 <div className="admin-database__logo-container">
@@ -24,30 +45,43 @@ function AdminDatabase() {
                             <li className="admin-database__nav-item">POSTCODE</li>
                         </ul>
                     </nav>
-                    <div className="admin-database__user-content-container admin-database__user-content-container--left">
-                        <div className="admin-database__item">
-                            <span className="admin-database__categories">
-                                FIRST NAME
-                            </span>
-                            <p className="admin-database__user-content">
-                                Charlie</p>
-                        </div>
-                        <div className="admin-database__item--right">
-                            <span className="admin-database__categories">POSTCODE</span>
-                            <p className="admin-database__user-content">
-                                TW12 5D6
-                            </p>
-                        </div>
-                    </div>
-                    <div className="admin-database__user-content-container">
-                        <div className="admin-database__item">
-                            <span className="admin-database__categories">
-                                EMAIL
-                            </span>
-                            <p className="admin-database__user-content">
-                                charlietait147@gmail.com</p>
-                        </div>
-                    </div>
+                    {userList.map((user) => {
+                        return (
+
+                            <div className="admin-database__user-content-mobile-container" key={user.id}>
+                                <div className="admin-database__user-content-container admin-database__user-content-container--left">
+                                    <div className="admin-database__item">
+                                        <span className="admin-database__categories">
+                                            FIRST NAME
+                                        </span>
+                                        <p className="admin-database__user-content">
+                                            {user.first_name}</p>
+                                    </div>
+                                    <div className="admin-database__item--right">
+                                        <span className="admin-database__categories">POSTCODE</span>
+                                        <p className="admin-database__user-content">
+                                            {user.postcode}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="admin-database__user-content-container">
+                                    <div className="admin-database__item">
+                                        <span className="admin-database__categories">
+                                            EMAIL
+                                        </span>
+                                        <p className="admin-database__user-content">
+                                            {user.email}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        )
+
+
+                    }
+
+                    )}
+
                 </div>
             </div>
         </section>
