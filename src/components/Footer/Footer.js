641b8614489d8b812302fbfa6abd5a619bd898e7
Charlie Tait <charlietait147@gmail.com>
2023-08-31 11:19:43 +0100

    add tablet, desktop breakpoints, add links and move inside item class

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,12 @@ function Footer() {
         <footer className = "footer-section">
             <div className="footer-section__wrapper">
                 <ul className="footer-section__list">
-                    <Link to = "contact-us" className = "footer-section__link"><li className="footer-section__item">
-                        Contact Us
-                    </li></Link>
-                    <Link to = "news" className = "footer-section__link"><li className="footer-section__item">
-                        News
-                    </li></Link>
+                    <li className="footer-section__item">
+                    <Link to = "contact-us" className = "footer-section__link">Contact Us
+                    </Link></li>
+                    <li className="footer-section__item">
+                    <Link to = "news" className = "footer-section__link">News
+                    </Link></li>
                 </ul>
                 <div className="footer-section__copyright-container">
                     <small className="footer-section__copyright">© 2023 Home EcoEarner Limited, All Rights Reserved</small>
