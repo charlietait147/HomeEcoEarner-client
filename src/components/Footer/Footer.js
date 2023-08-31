e047186dd6b445f7519d6b585cb91addae154de2
Charlie Tait <charlietait147@gmail.com>
2023-08-31 10:33:08 +0100

    make html template for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,19 @@
+import "./Footer.scss";
 function Footer() {
     return ( 
-        <div className="footer">This is the footer </div>
+        <footer className = "footer-section">
+            <div className="footer-section__wrapper">
+                <ul className="footer-section__list">
+                    <li className="footer-section__link">
+                        Contact Us
+                    </li>
+                    <li className="footer-section__link">
+                        News
+                    </li>
+                </ul>
+                <small className="footer-section__copyright">© 2023 Home EcoEarner Limited, All Rights Reserved</small>
+            </div>
+        </footer>
      );
 }
 
