7a91f2a8ef33b72a3e168b2fc132f8f3c76c21c4
Charlie Tait <charlietait147@gmail.com>
2023-08-31 20:58:07 +0100

    add header, footer to newspage, make newspage content component, add to newspage

---
+++ src/components/NewsContent/NewsContent.js
@@ -0,0 +1,7 @@
+function NewsContent() {
+    return ( 
+        <div className="news-content">This is the news content page</div>
+     );
+}
+
+export default NewsContent;
--- src/pages/NewsPage/NewsPage.js
+++ src/pages/NewsPage/NewsPage.js
@@ -1,7 +1,14 @@
 import "./NewsPage.scss";
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+import NewsContent from "../../components/NewsContent/NewsContent";
 function NewsPage() {
     return ( 
-        <div>This is the News Page</div>
+        <>
+        <NavBar />
+        <NewsContent />
+        <Footer />
+        </>
      );
 }
 
