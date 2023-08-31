9f39aca87d5bbdeed25da2386fca7804be19e4ef
Charlie Tait <charlietait147@gmail.com>
2023-08-31 13:54:26 +0100

    styles complete with breakpoints and links add to relevant pages

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,17 @@
 import "./NavBar.scss";
 import logo from "../../assets/images/homeecoearner-logo.png";
+import { Link } from "react-router-dom";
 function Header() {
     return ( 
-        <nav className="nav">
-            <div className="nav__wrapper">
-                <img src={logo} alt="Home Eco Earner logo" className="nav__logo" />
-                <ul className="nav__list">
-                    <li className="nav__link">NEWS</li>
-                    <li className="nav__link">CONTACT US</li>
+        <nav className="nav-bar">
+            <div className="nav-bar__wrapper">
+                <div className="nav-bar__logo-tagline-container">
+                    <img src={logo} alt="Home Eco Earner logo" className="nav-bar__logo" />
+                    <p className = "nav-bar__tagline" >Trusted Provider <span className = "nav-bar__tagline--green">of Green Energy</span></p>
+                </div>
+                <ul className="nav-bar__list">
+                    <li className="nav-bar__item"><Link to = "news" className = "nav-bar__link">News</Link></li>
+                    <li className="nav-bar__item"><Link to = "contact-us" className = "nav-bar__link">Contact Us</Link></li>
                 </ul>
             </div>
         </nav>
