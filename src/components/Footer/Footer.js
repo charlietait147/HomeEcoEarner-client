70b7d9cebb4b10d31e62ce633d12e7851949bad1
Charlie Tait <charlietait147@gmail.com>
2023-09-01 14:39:49 +0100

    add / in front of footer link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,10 +6,10 @@ function Footer() {
             <div className="footer-section__wrapper">
                 <ul className="footer-section__list">
                     <li className="footer-section__item">
-                    <Link to = "contact-us" className = "footer-section__link">Contact Us
+                    <Link to = "/contact-us" className = "footer-section__link">Contact Us
                     </Link></li>
                     <li className="footer-section__item">
-                    <Link to = "news" className = "footer-section__link">News
+                    <Link to = "/news" className = "footer-section__link">News
                     </Link></li>
                 </ul>
                 <div className="footer-section__copyright-container">
