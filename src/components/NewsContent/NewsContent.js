628840da172bd8cb7d1951d60c5be85cdc6c7409
Charlie Tait <charlietait147@gmail.com>
2023-09-01 11:23:37 +0100

    add html

--- src/components/NewsContent/NewsContent.js
+++ src/components/NewsContent/NewsContent.js
@@ -6,6 +6,25 @@ function NewsContent() {
                 <div className="news-content__banner">
                     <span className = "news-content__header">NEWS</span>
                 </div>
+                <div className="news-content__news-article-container">
+                    <div className="news-content__search-container">
+                        <p className="news-content__search-title">FILTER/SEARCH:</p>
+                        <input type="text" className="news-content__filter-search" />
+                        <button className = "news-content__filter-button">FILTER NOW</button>
+                    </div>
+                    <p className="news-content__results"> 6 Results </p>
+                    <div className="news-content__card-container">
+                            <article className="news-content__card">
+                            <img src="" alt="" className="news-content__image" />
+                            <p className="news-content__article-release-date">
+                            SEPTEMBER 1, 2023
+                            </p>
+                            <h3 className="news-content__article-title">
+                            Why Solar Panels are becoming more important than ever
+                            </h3>
+                        </article>
+                    </div>
+                </div>
             </div>
         </section>
 
