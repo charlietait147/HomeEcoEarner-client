93696e8cbcd6107b1e790247bbd3b3e26b0cbd6a
Charlie Tait <charlietait147@gmail.com>
2023-09-03 16:19:25 +0100

    make admindashboard oute, create page component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ContactPage from "./pages/ContactPage/ContactPage";
 import NewsPage from "./pages/NewsPage/NewsPage";
 import NotFound from "./pages/NotFound/NotFound";
 import QualifyForm from './pages/QualifyForm/QualifyForm';
-// import SuccessResponse from './pages/SuccessResponse/SuccessResponse';
+import AdminDashboardPage from './pages/AdminDashboardPage/AdminDashboardPage';
 
 function App() {
   return (
@@ -16,9 +16,9 @@ function App() {
     <Route path = "/" element = {<LandingPage />}/>
     <Route path = "qualify" element = {<QualifyForm />}/>
     <Route path = "admin/login" element = {<AdminLoginPage />}/>
+    <Route path = "admin/dashboard" element = {<AdminDashboardPage />}/>
     <Route path = "contact-us" element = {<ContactPage />}/>
     <Route path = "news" element = {<NewsPage />} />
-    {/* <Route path = "success" render={() => (isFormSubmitted ? <SuccessResponse/> : <Redirect to = "/" />)} /> */}
     <Route path = "*" element = {<NotFound />} /> 
     </Routes>
     </BrowserRouter>
---
+++ src/pages/AdminDashboardPage/AdminDashboardPage.js
@@ -0,0 +1,7 @@
+function AdminDashboardPage() {
+    return ( 
+        <div>This is the Admin Dashboard Page</div>
+     );
+}
+
+export default AdminDashboardPage;
