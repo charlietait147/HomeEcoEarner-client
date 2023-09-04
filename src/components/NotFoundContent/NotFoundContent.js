14eb4f84170215c386c210267d39c8bc1807dfb2
Charlie Tait <charlietait147@gmail.com>
2023-09-04 19:05:44 +0100

    add notfound html

--- src/components/NotFoundContent/NotFoundContent.js
+++ src/components/NotFoundContent/NotFoundContent.js
@@ -1,7 +1,14 @@
 import "./NotFoundContent.scss";
 function NotFoundContent() {
     return ( 
-        <div>This is the NotFoundContent Component!</div>
+        <section className="not-found">
+            <div className="not-found__wrapper">
+                <h1 className="not-found__header"> Oops!</h1>
+                <p className = "not-found__page-info">404 - PAGE NOT FOUND</p>
+                <p className = "not-found__page-description">The page you are looking for does not exist!</p>
+                <button className="not-found__home-button">GO TO HOMEPAGE</button>
+            </div>
+        </section>
      );
 }
 
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,12 +1,13 @@
 import "./NotFound.scss";
 import NavBar from "../../components/NavBar/NavBar"
 import Footer from "../../components/Footer/Footer";
+import NotFoundContent from "../../components/NotFoundContent/NotFoundContent";
 
 function NotFound() {
     return (
         <>
         <NavBar />
-        
+        <NotFoundContent />
         <Footer />
         </>
      );
