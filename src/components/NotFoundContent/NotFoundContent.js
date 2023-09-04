fa8e7249e4f8f18c8d1e3690a814c53865b1bd71
Charlie Tait <charlietait147@gmail.com>
2023-09-04 20:02:47 +0100

    add button functionality

--- src/components/NotFoundContent/NotFoundContent.js
+++ src/components/NotFoundContent/NotFoundContent.js
@@ -1,5 +1,13 @@
 import "./NotFoundContent.scss";
+import { useNavigate } from "react-router-dom";
 function NotFoundContent() {
+    const navigate = useNavigate()
+
+    const clickHandler = () => {
+        navigate("/");
+    }
+
+
     return ( 
         <section className="not-found">
             <div className="not-found__wrapper">
@@ -7,7 +15,7 @@ function NotFoundContent() {
                 <p className = "not-found__page-info">404 - PAGE NOT FOUND</p>
                 <p className = "not-found__page-description">The page you are looking for does not exist!</p>
                 <div className="not-found__home-button-container">
-                    <button className="not-found__home-button">GO TO HOMEPAGE</button>
+                    <button onClick = {clickHandler} className="not-found__home-button">GO TO HOMEPAGE</button>
                 </div>
             </div>
         </section>
