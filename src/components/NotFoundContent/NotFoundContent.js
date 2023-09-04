21aeaef025433b99210c99717455672a0cdb8912
Charlie Tait <charlietait147@gmail.com>
2023-09-04 19:57:32 +0100

    add styling, import background to use on text and button

--- src/components/NotFoundContent/NotFoundContent.js
+++ src/components/NotFoundContent/NotFoundContent.js
@@ -3,10 +3,12 @@ function NotFoundContent() {
     return ( 
         <section className="not-found">
             <div className="not-found__wrapper">
-                <h1 className="not-found__header"> Oops!</h1>
+                <div className="not-found__header"> Oops!</div>
                 <p className = "not-found__page-info">404 - PAGE NOT FOUND</p>
                 <p className = "not-found__page-description">The page you are looking for does not exist!</p>
-                <button className="not-found__home-button">GO TO HOMEPAGE</button>
+                <div className="not-found__home-button-container">
+                    <button className="not-found__home-button">GO TO HOMEPAGE</button>
+                </div>
             </div>
         </section>
      );
