c64d0bf2d513ee96f04112b1f929c2d0d5275482
Charlie Tait <charlietait147@gmail.com>
2023-10-30 15:38:45 +0000

    add menu icon and set up links to pages

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,37 +1,69 @@
 import "./NavBar.scss";
 import logo from "../../assets/images/HomeEcoEarner-edited-logo.png"
 import { Link, NavLink, useLocation } from "react-router-dom";
-import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
-import FeedOutlinedIcon from '@mui/icons-material/FeedOutlined';
+// import { useState } from "react";
+// import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
+// import FeedOutlinedIcon from '@mui/icons-material/FeedOutlined';
 function NavBar() {
     const location = useLocation();
+    // const [isMenuOpen, setIsMenuOpen] = useState(false);
+    // const [menuClass, SetMenuClass] = useState("nav-bar__menu")
+
+
+    // const toggleMenu = () => {
+    //     setIsMenuOpen(!isMenuOpen);
+    //     SetMenuClass( isMenuOpen ? "nav-bar__menu" : "nav-bar__close")
+    // }
     return (
         <nav className="nav-bar">
             <div className="nav-bar__wrapper">
                 <div className="nav-bar__logo-tagline-container">
-                    <Link to="/"><img src={logo} alt="Home Eco Earner logo" className="nav-bar__logo" /></Link>
-                    <p className="nav-bar__tagline" >Trusted Provider <span className="nav-bar__tagline--green">of Green Energy</span></p>
-                </div>
-                <ul className="nav-bar__list nav-bar__list--mobile">
-                    <div className="nav-bar__item-container">
-                       <FeedOutlinedIcon
-                            style={{ fontSize: "2.5rem", alignSelf: "center"}} onClick={() => window.location.replace('/news')}
-                        />
-                        <NavLink to="/news" className={location.pathname === "/news" ? "nav-bar__link-active" : "nav-bar__link"}>News</NavLink>
-                        </div>
-                    <div className="nav-bar__item-container">
-                       <MailOutlinedIcon
-                            style={{ fontSize: "2.5rem", alignSelf: "center"}} 
-                        />
-                        {/* <li className="nav-bar__item"><NavLink to = "/contact-us" className = {location.pathname === "/contact-us" ? "nav-bar__link-active" : "nav-bar__link"}>Contact Us</NavLink></li> */}
-                        <NavLink to = "mailto:rory.tait@homeecoearner.com" className="nav-bar__email-link">Email</NavLink>
+                    <Link to="/" className="nav-bar__logo-link"><img src={logo} alt="Home Eco Earner logo" className="nav-bar__logo" /></Link>
+                    {/* <p className="nav-bar__tagline" >Trusted Provider <span className="nav-bar__tagline--green">of Green Energy</span></p> */}
+                    <div className="nav-bar__menu-container">
+                        <button className="nav-bar__menu">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <ul className="nav-bar__dropdown-list">
+                                <a href="/" className={location.pathname === "/" ? "nav-bar__dropdown-link nav-bar__dropdown-link--active" : "nav-bar__dropdown-link"}>HOME</a>
+                                <a href="" className="nav-bar__dropdown-link">PRODUCTS</a>
+                                <a href="" className="nav-bar__dropdown-link">SERVICES</a>
+                                <a href="" className="nav-bar__dropdown-link">PARTNERSHIPS</a>
+                                <a href="" className="nav-bar__dropdown-link">NEWS</a>
+                                <a href="" className="nav-bar__dropdown-link">FAQs</a>
+                            </ul>
+                        </button>
+
+                        {/* <label className="nav-bar__menu-label">Menu</label> */}
                     </div>
-                </ul>
-                <ul className="nav-bar__list nav-bar__list--large-mobile">
-                        <NavLink to="/news" className={location.pathname === "/news" ? "nav-bar__link-active" : "nav-bar__link"}>News</NavLink>
+                    <ul className="nav-bar__list">
+                        
                         {/* <li className="nav-bar__item"><NavLink to = "/contact-us" className = {location.pathname === "/contact-us" ? "nav-bar__link-active" : "nav-bar__link"}>Contact Us</NavLink></li> */}
-                        <NavLink to = "mailto:rory.tait@homeecoearner.com" className="nav-bar__email-link nav-bar__email-link--large-mobile">rory.tait@homeecoearner.com</NavLink>
+                        {/* <NavLink to="mailto:rory.tait@homeecoearner.com" className="nav-bar__email-link nav-bar__email-link--large-mobile">rory.tait@homeecoearner.com</NavLink> */}
+                        <NavLink to="" className="nav-bar__link">Products</NavLink>
+                        <NavLink to="" className="nav-bar__link">Services</NavLink>
+                        <NavLink to="" className="nav-bar__link">Partnerships</NavLink>
+                        <NavLink to="/news" className={location.pathname === "/news" ? "nav-bar__link-active" : "nav-bar__link"}>News</NavLink>
+                        <NavLink to="" className="nav-bar__link">FAQS</NavLink>
                     </ul>
+                </div>
+                {/* <ul className="nav-bar__list nav-bar__list--mobile">
+                    <div className="nav-bar__item-container">
+                        <FeedOutlinedIcon
+                            style={{ fontSize: "2.5rem", alignSelf: "center" }} onClick={() => window.location.replace('/news')}
+                        />
+                        <NavLink to="/news" className={location.pathname === "/news" ? "nav-bar__link-active" : "nav-bar__link"}>News</NavLink>
+                    </div>
+                    <div className="nav-bar__item-container">
+                        <MailOutlinedIcon
+                            style={{ fontSize: "2.5rem", alignSelf: "center" }}
+                        />
+                        <li className="nav-bar__item"><NavLink to = "/contact-us" className = {location.pathname === "/contact-us" ? "nav-bar__link-active" : "nav-bar__link"}>Contact Us</NavLink></li>
+                        <NavLink to="mailto:rory.tait@homeecoearner.com" className="nav-bar__email-link">Email</NavLink>
+                    </div>
+                </ul> */}
+
 
             </div>
         </nav>
