d247d588e5a8f1dd17e5c91b619f837bb073985d
Charlie Tait <charlietait147@gmail.com>
2023-11-02 15:39:35 +0000

    add _redirects file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.scss';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import AdminLoginPage from "./pages/AdminLoginPage/AdminLoginPage";
-// import ContactPage from "./pages/ContactPage/ContactPage";
 import NewsPage from "./pages/NewsPage/NewsPage";
 import NotFound from "./pages/NotFound/NotFound";
 import AdminDashboardPage from './pages/AdminDashboardPage/AdminDashboardPage';
@@ -11,17 +10,16 @@ import AboutPage from './pages/AboutPage/AboutPage';
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-    <Route path = "/" element = {<LandingPage />}/>
-    <Route path = "admin/login" element = {<AdminLoginPage />}/>
-    <Route path = "admin/dashboard" element = {<AdminDashboardPage />}/>
-    {/* <Route path = "contact-us" element = {<ContactPage />}/> */}
-    <Route path = "news" element = {<NewsPage />} />
-    <Route path = "*" element = {<NotFound />} /> 
-    <Route path = "about" element = {<AboutPage /> } />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="admin/login" element={<AdminLoginPage />} />
+          <Route path="admin/dashboard" element={<AdminDashboardPage />} />
+          <Route path="news" element={<NewsPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
 
   );
