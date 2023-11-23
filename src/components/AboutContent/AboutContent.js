721577e64be4ed699110e7e318d9d4cb71990bf4
Charlie Tait <charlietait147@gmail.com>
2023-11-23 10:00:53 +0000

    redraft about page

--- src/components/AboutContent/AboutContent.js
+++ src/components/AboutContent/AboutContent.js
@@ -1,15 +1,40 @@
 import "./AboutContent.scss";
 import houses from "../../assets/images/houses.jpg";
 import trees from "../../assets/images/trees.jpg";
-
+import davidBio from "../../assets/images/david-bio.png";
+import roryBio from "../../assets/images/rory-bio.png";
 // import world from "../../assets/images/world-electricity.jpg";
 function AboutContent() {
     return (
         <main className="about-content">
-            <div className="about-content__banner">
+            {/* <div className="about-content__banner">
                 <span className="about-content__header">ABOUT</span>
-            </div>
+            </div> */}
             <div className="about-content__wrapper">
+                <h2 className="about-content__section-heading">Our Team</h2>
+                <div className="about-content__bio-container">
+                    <div className="about-content__bio">
+                        <div className="about-content__bio-image-container">
+                            <img src={roryBio} alt="headshot of Rory Tait" className="about-content__bio-image" />
+                        </div>
+                        <h3 className="about-content__bio-name">Rory Tait</h3>
+                        <p className="about-content__bio-description">Co-Founder / Director - Home EcoEarner Ltd, Secretary Association for Renewable
+                            Energy and Clean Technology (REA), Head of Renewables, Prospect Law. Former solicitor, 30
+                            years’ experience advising companies active in the energy sector, from developers of projects
+                            through to network operators on a wide range of clean energy technologies.</p>
+                    </div>
+                    <div className="about-content__bio">
+                        <div className="about-content__bio-image-container">
+                            <img src={davidBio} alt="headshot of David Sheldon" className="about-content__bio-image about-content__bio-image--modified" />
+                        </div>
+                        <h3 className="about-content__bio-name">David Sheldon</h3>
+                        <p className="about-content__bio-description">Co-Founder / Business Development Director - Home EcoEarner Ltd. 30
+                            years’ experience establishing businesses and arranging asset finance in the transport and
+                            renewable energy sectors. Accounting, Tax, Corporate advisory, Investment Banking, Asset
+                            finance / Leasing, Renewable energy, Solar, Wind, Nuclear.</p>
+                    </div>
+
+                </div>
                 <h2 className="about-content__section-heading">What Do We Do?</h2>
                 <div className="about-content__header-container">
                     <div className="about-content__image-container">
@@ -28,23 +53,24 @@ function AboutContent() {
                 </div>
             </div>
             <div className="about-content__container">
-                    <div className="about-content__wrapper about-content__wrapper--margin-left">
-                        <h3 className="about-content__section-subheading about-content__section-subheading--contrast">So they can:</h3>
-                        <ol className="about-content__numbered-list">
-                            <li className="about-content__numbered-item">Generate their own electricity.</li>
-                            <li className="about-content__numbered-item">Free themselves from electricity bills.</li>
-                            <li className="about-content__numbered-item">Free themselves from future electricity price increases.</li>
-                            <li className="about-content__numbered-item">Potentially earn a meaningful income from their home electricity generation.</li>
-                            <li className="about-content__numbered-item">All with no up-front cost.</li>
-                            <li className="about-content__numbered-item">While doing their bit to help the planet get to Net Zero.</li>
-                        </ol>
+                <div className="about-content__wrapper about-content__wrapper--margin-left">
+                    <h3 className="about-content__section-subheading about-content__section-subheading--contrast">So they can:</h3>
+                    <ol className="about-content__numbered-list">
+                        <li className="about-content__numbered-item">Generate their own electricity.</li>
+                        <li className="about-content__numbered-item">Free themselves from electricity bills.</li>
+                        <li className="about-content__numbered-item">Free themselves from future electricity price increases.</li>
+                        <li className="about-content__numbered-item">Potentially earn a meaningful income from their home electricity generation.</li>
+                        <li className="about-content__numbered-item">All with no up-front cost.</li>
+                        <li className="about-content__numbered-item">While doing their bit to help the planet get to Net Zero.</li>
+                    </ol>
                 </div>
                 <div className="about-content__background-image-container">
                     <img src={trees} alt="trees" className="about-content__background-image"></img>
                 </div>
             </div>
+
             {/* <img src={world} alt="world electricity" className="about-content__world-electricity-image" /> */}
-            <div className="about-content__wrapper">
+            {/* <div className="about-content__wrapper">
                 <h2 className="about-content__section-heading">How Do We Do It?</h2>
             </div>
             <div className="about-content__banner-proposition">
@@ -65,7 +91,7 @@ function AboutContent() {
 
                 </ul>
             </div>
-
+ */}
 
 
         </main>
