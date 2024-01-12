5ffd623caab700ac13779d97d488cb9f6b4f7671
Charlie Tait <charlietait147@gmail.com>
2024-01-12 20:33:16 +0000

    add contact info

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -49,69 +49,77 @@ function ContactForm() {
         <span className="contact-form__header">CONTACT US</span>
       </div> */}
       <div className="contact-form__wrapper">
-        <div className="contact-form__title-container">
-          <p className="contact-form__title">
-            Need to get in touch with us? Fill out the form with your
-            enquiry.
-          </p>
-          <p className="contact-form__address">2 Brunel Place, Slough, SL1 1FQ, UK</p>
-        </div>
-        <div className="contact-form__form-container">
-          <CForm action="https://formsubmit.co/info@homeecoearner.com" method="POST" className="contact-form__form" >
-            {/* <CForm onSubmit={submitHandler} className="contact-form__form" > */}
-            <CFormLabel className="contact-form__form-label" htmlFor="name">
-              First name*
-            </CFormLabel>
-            <CFormInput
-              type="text"
-              size="lg"
-              className="contact-form__form-input"
-              id="name"
-              name="name"
-              required
-              onChange={handleChangeName}
-              value={name}
-            />
-            <CFormLabel className="contact-form__form-label" htmlFor="email">
-              Email*
-            </CFormLabel>
-            <CFormInput
-              type="text"
-              size="lg"
-              className="contact-form__form-input"
-              id="email"
-              name="email"
-              required
-              onChange={handleChangeEmail}
-              value={email}
+        <div className="contact-form__section-container">
+          <div className="contact-form__title-container">
+            <p className="contact-form__title">
+              Need to get in touch with us? Fill out the form with your
+              enquiry.
+            </p>
 
-            />
-            <CFormLabel
-              className="contact-form__form-label"
-              htmlFor="description"
-            >
-              What can we help you with?
-            </CFormLabel>
-            <CFormTextarea
-              type="text"
-              size="lg"
-              className="contact-form__form-description"
-              id="description"
-              name="description"
-              required
-              onChange={handleChangeDescription}
-              value={description}
-            />
-            <div className="contact-form__submit-button-container">
-              <button
-                type="submit"
-                className={isButtonDisabled ? "contact-form__submit-button contact-form__submit-button--disabled" : "contact-form__submit-button"}
-                onClick={() => setIsLoading(true)}
-                disabled={isButtonDisabled}
-              > {isLoading ? "Loading..." : "Submit"}</button>
-            </div>
-          </CForm>
+          </div>
+          <div className="contact-form__form-container">
+            <CForm action="https://formsubmit.co/info@homeecoearner.com" method="POST" className="contact-form__form" >
+              {/* <CForm onSubmit={submitHandler} className="contact-form__form" > */}
+              <CFormLabel className="contact-form__form-label" htmlFor="name">
+                First name*
+              </CFormLabel>
+              <CFormInput
+                type="text"
+                size="lg"
+                className="contact-form__form-input"
+                id="name"
+                name="name"
+                required
+                onChange={handleChangeName}
+                value={name}
+              />
+              <CFormLabel className="contact-form__form-label" htmlFor="email">
+                Email*
+              </CFormLabel>
+              <CFormInput
+                type="text"
+                size="lg"
+                className="contact-form__form-input"
+                id="email"
+                name="email"
+                required
+                onChange={handleChangeEmail}
+                value={email}
+
+              />
+              <CFormLabel
+                className="contact-form__form-label"
+                htmlFor="description"
+              >
+                What can we help you with?
+              </CFormLabel>
+              <CFormTextarea
+                type="text"
+                size="lg"
+                className="contact-form__form-description"
+                id="description"
+                name="description"
+                required
+                onChange={handleChangeDescription}
+                value={description}
+              />
+              <div className="contact-form__submit-button-container">
+                <button
+                  type="submit"
+                  className={isButtonDisabled ? "contact-form__submit-button contact-form__submit-button--disabled" : "contact-form__submit-button"}
+                  onClick={() => setIsLoading(true)}
+                  disabled={isButtonDisabled}
+                > {isLoading ? "Loading..." : "Submit"}</button>
+              </div>
+            </CForm>
+          </div>
         </div>
+        <p className="contact-form__title">Or get in touch with us:</p>
+          <div className="contact-form__info-container">
+            <p className="contact-form__info">Phone: <b>020 3286 5350</b></p>
+            <p className="contact-form__info">Email: <b>info@homeecoearner.com</b></p>
+            <p className="contact-form__info">Address:<b> 2 Brunel Place, Slough, SL1 1FQ, UK</b></p>
+          </div>
       </div>
     </section>
   );
